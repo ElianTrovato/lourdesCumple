073124579bbf216f4ffdabcbd5d74c37f32b81e3
Elian Trovato <eliantrovatohotmail.com>
2021-11-04 01:17:27 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let volumen = alert("Subir el Volumen por favor")
+let verificacion = prompt("Subiste el volumen Che?")
+
+$("#galeria").click();
+                $("#galeria").click(function() {
+                $("#fotos").toggle();
+                alert("Deslizar hacia abajo")
+            })
+
+$("#noCantar").click();
+            $("#noCantar").click(function() {
+            $("#cantar").toggle();
+            })
+
+$("#buscarRegalos").click();
+            $("#buscarRegalos").click(function() {
+            $("#regalos").toggle();
+        })
