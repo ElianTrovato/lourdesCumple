f95be7619859c4b70ac8d9635bc676f3e5fc7679
Elian Trovato <eliantrovatohotmail.com>
2021-11-05 01:12:44 -0300

    first commit

--- index.js
+++ index.js
@@ -1,18 +1,82 @@
-let volumen = alert("Subir el Volumen por favor")
+let volumen = alert("Subir el Volumen por favor e iniciar el video")
 let verificacion = prompt("Subiste el volumen Che?")
+let obligacion = alert("Inicia el video y NO SIGAS HASTA QUE TERMINE")
+
+$("#regalos").click();
+            $("#regalos").click(function() {
+            $("#regalo").toggle();
+        })
+
 
 $("#galeria").click();
                 $("#galeria").click(function() {
-                $("#fotos").toggle();
+                $("#foto1").toggle();
                 alert("Deslizar hacia abajo")
             })
 
-$("#noCantar").click();
-            $("#noCantar").click(function() {
-            $("#cantar").toggle();
-            })
 
-$("#buscarRegalos").click();
-            $("#buscarRegalos").click(function() {
-            $("#regalos").toggle();
-        })
+$(".btn-flotante").click();
+            $(".btn-flotante").click(function(){
+                $("#foto2").show();
+            });
+
+ $(".btn-flotante2").click();
+            $(".btn-flotante2").click(function(){
+                $("#foto3").show();
+            });
+
+$(".btn-flotante3").click();
+            $(".btn-flotante3").click(function(){
+                $("#foto4").show();
+            });
+
+$(".btn-flotante4").click();
+            $(".btn-flotante4").click(function(){
+                $("#foto5").show();
+            });
+        
+$(".btn-flotante5").click();
+            $(".btn-flotante5").click(function(){
+                $("#foto6").show();
+            });
+
+$(".btn-flotante6").click();
+            $(".btn-flotante6").click(function(){
+                $("#foto7").show();
+            });
+
+$(".btn-flotante7").click();
+            $(".btn-flotante7").click(function(){
+                $("#foto8").show();
+            });
+
+$(".btn-flotante8").click();
+            $(".btn-flotante8").click(function(){
+                $("#foto9").show();
+            });
+
+$(".btn-flotante9").click();
+            $(".btn-flotante9").click(function(){
+                $("#foto10").show();
+            });
+
+$(".btn-flotante10").click();
+            $(".btn-flotante10").click(function(){
+                $("#foto11").show();
+            });
+
+$(".btn-flotante11").click();
+            $(".btn-flotante11").click(function(){
+                $("#foto12").show();
+            });
+
+$(".btn-flotante12").click();
+            $(".btn-flotante12").click(function(){
+                $("#foto13").show();
+            });
+
+$(".btn-flotante13").click();
+            $(".btn-flotante13").click(function(){
+                $("#foto14").show();
+            });
+
